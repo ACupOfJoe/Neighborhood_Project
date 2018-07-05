22509e019bb6616898dd3c3335dada706265c47c
Joseph <joevasquez927@live.com>
2018-07-05 15:40:10 -0700

    Hello World

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+
+	var center;
+	function calculateCenter() {
+	center = map.getCenter();
+	}
+	google.maps.event.addDomListener(map, 'idle', function() {
+		calculateCenter();
+	});
+	google.maps.event.addDomListener(window, 'resize', function() {
+		map.setCenter(center);
+	});
+}
