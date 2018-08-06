23112375f57d1d0116329f4d63ee39dd73993fd8
Joseph Paul Vasquez <joevasquez927@Live.com>
2018-08-06 15:52:01 -0700

    Remove run.js

--- js/run.js
+++
@@ -1,8 +0,0 @@
-var url = "https://maps.googleapis.com/maps/api/js?libraries=places,geometry&key=AIzaSyAxnOiuoL4eGMJ-HiCIRKPy3Jm-tBOSDW0&callback=execute"
-
-$.getScript(url)
-	.done(function() {
-	})
-	.fail(function() {
-		alert("Sorry. The Script Failed to Load");
-	});
